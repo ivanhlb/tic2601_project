b2b4f71560e8dfb793572991ee4e14028975e9a8
ivanhlb <ivanhlb@hotmail.com>
2019-11-12 13:00:20 +0800

    Working version for Azure Web Services, use pool for SQL.

--- index.js
+++ index.js
@@ -10,30 +10,43 @@ app.use(express.static(__dirname + '/public')); //allows use of stuff in ./publi
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-const port = 3000;
+const port = process.env.PORT || "3000";   //changed to this as the website is now https
 
 const mysql = require('mysql');
-const connection = mysql.createConnection({
+const sql_pw = 'baT7d()' + '\\' +'X!:h';    // raw: 'baT7d()\X!:h'
+var azure_mysql_str = {
+    connctionLimit: 5,
+    host: "tic2601-ticketing.mysql.database.azure.com",
+    user: "su@tic2601-ticketing",
+    password: sql_pw,
+    database: 'app', 
+    port: 3306, 
+    ssl: true
+};
+const local_mysql_str = {
+    connctionLimit: 5,
     host: 'localhost',
     user: 'root',
-    //   password: 'TIC2601',
+    // user: 'su',
+    //   password: 'baT7d()\X!:h',
     database: 'app'
-});
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected!');
-});
+};
+const pool = mysql.createPool(azure_mysql_str);
+// const connection = mysql.createConnection(azure_mysql_str);
+// connection.connect((err) => {
+//     if (err) throw err;
+//     console.log('Connected!');
+// });
 
-function renderMainPage(res) {
-    connection.query(
+function renderMainPage(res, found) {
+    pool.query(
         'SELECT City, Country from City ORDER BY City', (err, result, fields) => {
             if (err) {
                 console.log(err);
-                res.render('404');  //when submit form.
+                res.render('error', { err: 404 });  //when submit form.
             }
             else {
-                console.dir(result);
-                res.render('main', { 'countries': result });
+                res.render('main', { 'countries': result, 'found':found });
             }
         }
     )
@@ -45,7 +58,7 @@ app.get('/', (req, res) => {
     //https://www.npmjs.com/package/memory-cache
     //https://medium.com/the-node-js-collection/simple-server-side-cache-for-express-js-with-node-js-45ff296ca0f0
     //also need to validate country input while ensuring that input is not autofilled
-    renderMainPage(res);
+    renderMainPage(res, true);
 });
 app.post('/search', (req, res) => {
     let param = {};
@@ -61,10 +74,10 @@ app.post('/search', (req, res) => {
     toCity = param.to.split(', ');
     //flight_id, price, depart_time and arrival time.
     sqlquery =
-        "SELECT f.FlightID, f.Route, f.BasePrice, r.DepartureDatetime, r.ArrivalDatetime FROM Flight f INNER JOIN Route r ON f.Route = r.Route WHERE r.DepartureCity = '" + fromCity[0]
+        "SELECT f.FlightID, a.AirlineName, f.Route, f.BasePrice, r.DepartureDatetime, r.ArrivalDatetime FROM Flight f INNER JOIN Route r ON f.Route = r.Route RIGHT JOIN Airline a ON f.AirlineID = a.AirlineID WHERE r.DepartureCity = '" + fromCity[0]
         + "' and r.ArrivalCity = '" + toCity[0] + "' and r.DepartureDatetime LIKE '%"
-        + startDateStr + "%'";
-    connection.query(sqlquery, (err, result, fields) => {
+        + startDateStr + "%' ORDER BY f.BasePrice ASC LIMIT 5"; //limiting to 5 as the db is overpopulated.
+    pool.query(sqlquery, (err, result, fields) => {
         if (err) {
             console.log(err);
             res.render('500');
@@ -72,19 +85,19 @@ app.post('/search', (req, res) => {
         else {
             if (result.length == 0) {
                 console.warn("no arrival flights found!");
+                renderMainPage(res, false);
             }
             else {
                 console.log('found arrival flights.');
-                // console.dir(result);
                 param.firstFlights = result;
 
                 if (param.flightType != "oneway") {     //if return flight
                     console.log("checking return flights.");
                     sqlquery =
-                        "SELECT f.FlightID, f.Route, f.BasePrice, r.DepartureDatetime, r.ArrivalDatetime FROM Flight f INNER JOIN Route r ON f.Route = r.Route WHERE r.DepartureCity = '" + toCity[0]
+                        "SELECT f.FlightID, a.AirlineName, f.Route, f.BasePrice, r.DepartureDatetime, r.ArrivalDatetime FROM Flight f INNER JOIN Route r ON f.Route = r.Route RIGHT JOIN Airline a ON f.AirlineID = a.AirlineID WHERE r.DepartureCity = '" + toCity[0]
                         + "' and r.ArrivalCity = '" + fromCity[0] + "' and r.DepartureDatetime LIKE '%"
-                        + startDateStr + "%'";
-                    connection.query(sqlquery, (err, next_result, fields) => {
+                        + returnDateStr + "%' ORDER BY f.BasePrice ASC LIMIT 5"; //limiting to 5 as the db is overpopulated.
+                    pool.query(sqlquery, (err, next_result, fields) => {
                         if (err) {
                             console.log(err);
                             res.render('500');
@@ -94,11 +107,9 @@ app.post('/search', (req, res) => {
                             }
                             else {
                                 console.log('found return flights.');
-                                // console.dir(next_result);
+                                // console.dir(next_result[0]);
                                 param.nextFlights = next_result;
-                                console.log(param.nextFlights[0]);
                                 res.render('flightslists', param);
-                            
                             }
                         }
                     })
@@ -113,9 +124,13 @@ app.post('/search', (req, res) => {
 //403 forbidden, 404 not found, 500 internal server error
 
 app.post('/particulars', (req, res) => {    //this is for inserting your personal particulars once you decide which flight to take.
-    console.dir(req.body);
+    depart_arr = req.body.depart.split(',')
+    return_arr = req.body.return.split(',')
     //get results from plane.
+    console.log(req.body);
+    pool.query("", (err, result, fields) => {
 
+    })
     res.render('passenger_details', { planes: null });
 }); //to implement
 app.get('/test', (req, res) => {
@@ -126,7 +141,7 @@ app.post('/admin', (req, res) => { res.render('admin_panel') }); //to implement
 
 
 app.get('/account', (req, res) => {
-    connection.query(
+    pool.query(
         'SELECT * from country ORDER BY name', (err, result, fields) => {
             if (err) {
                 console.log(err);
@@ -142,7 +157,7 @@ app.post('/account', (req, res) => {
     console.dir(req.body);
     if (req.body.email != null) {
         try {
-            connection.query(
+            pool.query(
                 'SELECT customer_id from customer where email=' + req.body.email
             )
         } catch (error) {
@@ -151,7 +166,7 @@ app.post('/account', (req, res) => {
     }
     else {
         try {
-            connection.query(
+            pool.query(
                 'SELECT customer_id from customer where firstname='
                 + req.body.firstname + 'and lastname=' + req.body.lastname, (err, result, fields) => {
 
@@ -162,11 +177,11 @@ app.post('/account', (req, res) => {
         }
     }
     var constraint = (req.body.email != null) ? "email=" + req.body.email : "firstname=" + req.body.firstname + " and lastname=" + req.body.lastname;
-    connection.query(
+    pool.query(
         'SELECT * from booking WHERE bookingID=' + req.body.bookingID + 'and ' + constraint, (err, result, fields) => {
             if (err) {
                 console.log(err);
-                res.render('404');  //when submit form.
+                res.render('error', { err: 404 });  //when submit form.
             }
             else {
                 res.render('account', { countries: result });
